85ee463158f1d8c7c2a7ed579d54c42be27ca9f3
MarinaKam <marina.stozhok@gmail.com>
2015-10-06 19:51:10 +0300

    codewars

---
+++ GoIT/JavaScript/home/lessons2/codewars1.js
@@ -0,0 +1,6 @@
+/**
+ * Created by MarinaK on 06.10.15.
+ */
+function numberToString(num) {
+    return ''+num;// Return a string of the number here!
+}
---
+++ GoIT/JavaScript/home/lessons2/codewars2.js
@@ -0,0 +1,7 @@
+/**
+ * Created by MarinaK on 06.10.15.
+ */
+var stringToNumber = function(str){
+    return +str;// put your code here
+    //return null;
+}
