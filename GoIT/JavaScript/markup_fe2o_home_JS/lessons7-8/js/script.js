c7723e370efc8b8721e8094450474160424017d2
MarinaKam <marina.stozhok@gmail.com>
2016-01-29 18:23:41 +0200

    changed 7-8 les tooltip

--- GoIT/JavaScript/markup_fe2o_home_JS/lessons7-8/js/script.js
+++ GoIT/JavaScript/markup_fe2o_home_JS/lessons7-8/js/script.js
@@ -13,105 +13,32 @@ $(function () {
 
     $tabContent.first().show();
 
-    $('.tabs li a').on('click', function () {
+    $('.tabs a').on('click', function () {
         $(this).addClass('active').parents().siblings(this).find('a').removeClass('active');   
     });
 
 
-    var $form = $('.l-form');
-    $form.on('click', '.b-button-link a', function(e) {
-        e.preventDefault();
-        $('.hint1').fadeIn();
-        $('.hint2').fadeIn('slow');
-        $('.hint3').fadeIn(1000);
-    });
+    var $fixedToolTip = $('.fixedToolTip');
 
-    // var $hint = $('.hint input');
+    function hintIn() {
+        var left = $(this).position().left + $(this).width() + 50;
+        var top = $(this).position().top - 4;
+        $(this).next().css('left',left);
+        $(this).next().css('top',top);
+        $('.tooltip:last').css('left', left + 10);
+        $(this).next('.tooltip').animate({ opacity: "show", marginLeft: "10"}, "slow");
 
+    };
 
-    // function hoverIn() {
-    //     var $arr = ['.first-name', '.last-name', '.address'];
-    //     for (var i = 0; i < $arr; i++) {
-    //         $arr[i].fadeIn('fast');
-    //     };
-    // }
+    function hintOut() {
+        $(this).next('.tooltip').animate({opacity: "hide", marginLeft: "40"}, "fast");
+    };
 
-    // function hoverOut() {
-    //     var $arr = ['.first-name', '.last-name', '.address'];
-    //     for (var i = 0; i < $arr; i++) {
-    //         $arr[i].fadeOut('fast');
-    //     };
-    // }
+    $fixedToolTip.mouseover(hintIn).mouseout(hintOut);
 
-    // $('#hint-first-name').hover(hoverIn(), hoverOut());
-    // $('#hint-last-name').hover(hoverIn(),hoverOut());
-    // $('#hint-address').hover(hoverIn(),hoverOut());
-
-
-     // function hoverIn() {
-     //    $(this).find("em").stop(true,true)
-     //                      .animate({opacity: "show", top: "-75"}, "slow");
-     //                      console.log('em');
-     // }
-
-     // function hoverOut() {
-     //    $(this).find("em").stop(true,true)
-     //                      .animate({opacity: "hide", top: "-85"}, "fast");
-     // }
-
-     // var $hint = $('.hint input');
-     // console.log($hint);
-
-     // $hint.hover(hoverIn(), hoverOut());
-
-    //  var $em = $('.hint em');
-    //  console.log($em);
-
-    // $('.hint input').hover(function() {
-    //     $('.hint em').fadeIn('fast');
-    //     }, function() {
-    //     $('.hint em').fadeOut('fast');
-    // });
-
-    // var $hoverIn = function () {
-    //     for (var i = 0; i < arguments.length; i++) {
-    //     $('.hint').fadeIn('fast');
-    //     };
-    //     console.log('.hint' + (i+1));
-    // }
-
-    // var $hoverOut = function() {
-    //     for (var i = 0; i < arguments.length; i++) {
-    //     $('.hint').fadeOut('fast');
-    //     };
-    // }
-
-    // $('#hint-first-name').hover($hoverIn, $hoverOut);
-    // $('#hint-last-name').hover($hoverIn, $hoverOut);
-    // $('#hint-address').hover($hoverIn, $hoverOut);
-
-
-
-    $('#hint-first-name').hover(function() {
-        $('.hint1').fadeIn('fast');
-    }, function() {
-        $('.hint1').fadeOut('fast');
-      });
-
-    $('#hint-last-name').hover(function() {
-        $('.hint2').fadeIn('fast');
-    }, function() {
-        $('.hint2').fadeOut('fast');
-
-    });
-
-    $('#hint-address').hover(function() {
-        $('.hint3').fadeIn('fast');
-    }, function() {
-        $('.hint3').fadeOut('fast');
-
-    });
-
-   
+    $('.b-button').click(function(event) {
+        $('.fixedToolTip').next('.tooltip').animate({opacity: "show", marginLeft: "20"}, "slow");
+        event.preventDefault();
+    }); 
 
 });
