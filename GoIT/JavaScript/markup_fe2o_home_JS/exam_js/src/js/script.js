40e1f98e9dc1d2007bef72be13e69be693e49beb
Marina Kam <marina.stozhok@gmail.com>
2016-04-02 02:48:32 +0300

    ie style

--- GoIT/JavaScript/markup_fe2o_home_JS/exam_js/src/js/script.js
+++ GoIT/JavaScript/markup_fe2o_home_JS/exam_js/src/js/script.js
@@ -36,55 +36,61 @@ $(function () {
         touch ? 'touch-yes' : 'touch-no'
     );
 
-
-
     $('.owl-prev').html(' ');
     $('.owl-next').html(' ');
 
     var pic = '';
+    var dataForModal = {
+        images: []
+    };
 
 
     function renderList(pic) {
 
-        $.ajax({
-            type: 'GET',
-            dataType: 'json',
-            cache: false,
-            url: 'http://api.pixplorer.co.uk/image?word=' + pic + '&amount=7&size=m',
-            success: function(data) {
+        var index = 7;
+        for (var i = 0; i < index; i++) {
 
-                var piclist = tmpl($('#template').html(), data);
+            var f = function(i) {
 
-                $('.grid').remove();
+                $.ajax({
+                    type: 'GET',
+                    dataType: 'json',
+                    cache: false,
+                    url: 'http://api.pixplorer.co.uk/image?word=' + pic + '&amount=7&size=m',
+                    success: function(data) {
 
-                $('.content_gallery').append(piclist);
+                        dataForModal.images.push(data.images[0]);
 
-                $('.grid').isotope({
-                    itemSelector: '.grid_item',
-                    layoutMode: 'masonry',
-                    masonry: {
-                        gutter: 20
+                        if (dataForModal.images.length > index) {
+                            dataForModal.images.splice(0, 1);
+                        }
+                        if (i == (index - 1)) {
+                            setTimeout(function() {
+
+                                var piclist = tmpl($('#template').html(), dataForModal);
+
+                                $('.grid').remove();
+
+                                $('.content_gallery').append(piclist);
+                                $('.grid').isotope({
+                                    itemSelector: '.grid__item',
+                                    layoutMode: 'masonry',
+                                    masonry: {
+                                        gutter: 20
+                                    }
+                                });
+                            }, 500);
+                        }
+
                     }
                 });
 
-                $('.modal').hide();
+            }(i);
+        }
 
-                $('.grid_item').on('click', showModal);
-
-                function showModal(e){
-                    var targIndex = $(this).index();
-                    var modalURL = data.images[targIndex].imageurl;
-
-                    $('.modal-image').attr('src', modalURL);
-                    $('.modal').show();
-                }
-
-
-            }
-        });
     }
 
-
+    renderList();
 
     $('.form').submit(function(e) {
 
@@ -94,11 +100,24 @@ $(function () {
 
     });
 
-    renderList();
-    $('.modal-image').on('click', hideModal);
+    function showModal() {
+        var index = $(this).index();
+        var $modal = $('<div class="modal animated zoomIn"><div class="modal__wrap"><img class="modal__image" src="' + dataForModal.images[index].imageurl + '" alt=""> </div></div>');
+        var $overlay = $('<div class="modal__overlay"></div>');
 
-    function hideModal(){
-        $('.modal').hide();
+        $('body').append($modal);
+        $('body').append($overlay);
+
+        $modal.one('click', hideModal);
+
+        function hideModal() {
+            $modal.removeClass('zoomIn').addClass('zoomOut');
+            setTimeout(function() {
+                $modal.remove();
+            }, 500);
+            $overlay.remove();
+        }
     }
+    $('.content_gallery').on('click', '.grid__item', showModal);
 
 });
